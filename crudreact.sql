bc738c5e6c46382fff1745ad2f6769043b87c539
Angel lemus <lemusangel1092@hotmail.com>
2021-10-27 17:38:47 -0500

    Agregue el sql para crear la base de datos

---
+++ crudreact.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-10-2021 a las 00:36:57
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crudreact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `frameworks`
+--
+
+CREATE TABLE `frameworks` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `lanzamiento` int(10) NOT NULL,
+  `desarrollador` varchar(50) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `frameworks`
+--
+
+INSERT INTO `frameworks` (`id`, `nombre`, `lanzamiento`, `desarrollador`) VALUES
+(1, 'Angular', 2016, 'Google'),
+(2, 'React', 2013, 'Facebook'),
+(3, 'Vue', 2014, 'Evan You'),
+(4, 'Ember', 2011, 'Yehuda Katz'),
+(5, 'JS', 1990, 'John'),
+(6, 'Polymer', 2015, 'Google');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `frameworks`
+--
+ALTER TABLE `frameworks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `frameworks`
+--
+ALTER TABLE `frameworks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
