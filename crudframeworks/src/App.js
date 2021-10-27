7a865eb4dfb7f0bdabc60d6df914c414cffdb3bd
Lemus <lemusangel1092@hotmail.com>
2021-10-27 12:34:25 -0500

    Se añadio todo el modulo para agregar nuevos frameworks

--- crudframeworks/src/App.js
+++ crudframeworks/src/App.js
@@ -6,17 +6,54 @@ import axios from 'axios';
 function App() {
   const baseUrl = 'http://localhost/crudreact/apiframeworks/';
   const [data, setData] = useState([]);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [frameworkSeleccionado, setframeworkSeleccionado] = useState({
+    id: '',
+    nombre: '',
+    lanzamiento: '',
+    desarrollador: '',
+  });
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setframeworkSeleccionado((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(frameworkSeleccionado);
+  };
+  const abrirCerrarModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
+  };
+
+  const peticionPost = async () => {
+    var f = new FormData();
+    f.append('nombre', frameworkSeleccionado.nombre);
+    f.append('lanzamiento', frameworkSeleccionado.lanzamiento);
+    f.append('desarrollador', frameworkSeleccionado.desarrollador);
+    f.append('METHOD', 'POST');
+    await axios.post(baseUrl, f).then((response) => {
+      setData(data.concat(response.data));
+      abrirCerrarModalInsertar();
+    });
+  };
   const peticionGet = async () => {
-    await axios.get().then((response) => {
+    await axios.get(baseUrl).then((response) => {
       setData(response.data);
     });
   };
+
   useEffect(() => {
     peticionGet();
   }, []);
 
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
+      <button
+        className="btn btn-primary"
+        onClick={() => abrirCerrarModalInsertar()}
+      >
+        Insertar
+      </button>
       <table className="table table-striped">
         <thead>
           <tr>
@@ -41,8 +78,54 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={modalInsertar}>
+        <ModalHeader>Insertar Framework</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nombre: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nombre"
+              onChange={handleChange}
+            />
+            <br />
+            <label>Lanzamiento: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="lanzamiento"
+              onChange={handleChange}
+            />
+            <br />
+            <label>Desarrollador: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="desarrollador"
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => peticionPost()}>
+            Insertar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => abrirCerrarModalInsertar()}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
-/*Ver que apsa con el git as */
+
 export default App;
